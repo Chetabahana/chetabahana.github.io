19954459e7f0b73442747358c9aeb1527005f6c9
eq19 <eq19@users.noreply.github.com>
2025-07-27 05:00:44 +0000

    update workflows

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -212,9 +212,6 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
     done
   done
 
-  echo "🚀 All files updated (forced overwrite)!"
-  exit 0
-
   # Setup freqtrade config.json
   CONFIG="/home/runner/user_data/config.json"
   CONFIG_DRY="/home/runner/data_dry/config.json"
@@ -236,6 +233,7 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
     /mnt/disks/deeplearning/usr/bin/docker exec mydb sed -i "s|your_telegram_token|$TRADING_BOT_TOKEN|g" $CONFIG_LIVE
     /mnt/disks/deeplearning/usr/bin/docker exec mydb sed -i "s|user_data/strategies|/home/runner/data_dry/strategies|g" $CONFIG_DRY
     /mnt/disks/deeplearning/usr/bin/docker exec mydb sed -i "s|user_data/strategies|/home/runner/data_live/strategies|g" $CONFIG_LIVE
+    echo "🚀 All files updated (forced overwrite)!"
   fi
 
   /mnt/disks/deeplearning/usr/bin/docker exec mydb \
@@ -263,6 +261,8 @@ elif [[ "${JOBS_ID}" == "3" ]]; then
 
   /mnt/disks/deeplearning/usr/bin/docker exec mydb cp $HYPEROPT_PARAM /home/runner/data_dry/strategies/hyperopt_params.json
   /mnt/disks/deeplearning/usr/bin/docker exec mydb cp $HYPEROPT_PARAM /home/runner/data_live/strategies/hyperopt_params.json
+  /mnt/disks/deeplearning/usr/bin/docker exec mydb ls -alR /home/runner/data_dry
+  /mnt/disks/deeplearning/usr/bin/docker exec mydb ls -alR /home/runner/data_live
 
   echo -e "\n$hr\nCONFIG\n$hr" && cat _config.yml
   echo -e "\n$hr\nENVIRONTMENT\n$hr" && printenv | sort
